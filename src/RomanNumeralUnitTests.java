b805176a533093a5a8e4ebb8c4f7f43ecb7bfc64
yuvalzargari <yuvalzargari@gmail.com>
2012-12-16 10:30:59 +0200

    GREEN

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -30,8 +30,8 @@ public class RomanNumeralUnitTests {
 //		assertEquals("C", number.toRoman(100));
 //		assertEquals("CC", number.toRoman(200));
 //		assertEquals("CCL", number.toRoman(250));
-		assertEquals("MD", number.toRoman(1500));
-//		assertEquals("MMM", number.toRoman(3000));
+//		assertEquals("MD", number.toRoman(1500));
+		assertEquals("MMM", number.toRoman(3000));
 	}
 	
 
