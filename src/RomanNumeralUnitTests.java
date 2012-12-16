638aeaa771545091d2d345e057c2474d8395f92e
yuvalzargari <yuvalzargari@gmail.com>
2012-12-16 10:24:16 +0200

    RED

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -23,8 +23,8 @@ public class RomanNumeralUnitTests {
 		
 //		assertEquals("I", number.toRoman(1));
 //		assertEquals("II", number.toRoman(2));
-		assertEquals("III", number.toRoman(3));
-//		assertEquals("IV", number.toRoman(4));
+//		assertEquals("III", number.toRoman(3));
+		assertEquals("IV", number.toRoman(4));
 //		assertEquals("V", number.toRoman(5));
 		
 //		assertEquals("C", number.toRoman(100));
