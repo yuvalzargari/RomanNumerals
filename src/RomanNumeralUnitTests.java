c6bf6e5edeedc6a7c0a53f7eb8e9973002c20b16
yuvalzargari <yuvalzargari@gmail.com>
2012-12-16 10:21:52 +0200

    RED

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -9,8 +9,29 @@ public class RomanNumeralUnitTests {
 
 	@Test
 	public void testThat1ReturnsI() {
-		assertEquals("I", number.toRoman(1));
+//		assertEquals("I", number.toRoman(1));
+//		assertEquals("II", number.toRoman(2));
+//		assertEquals("X", number.toRoman(10));
+//		assertEquals("XLVIII", number.toRoman(48));
+//		assertEquals("MM", number.toRoman(2000));
+//		assertEquals("MCC", number.toRoman(1200));
+//		assertEquals("CMXCIX", number.toRoman(999));
+//		assertEquals("MCCXXXIV", number.toRoman(1234));
+//		//System.out.println(number.toRoman(48));
+//		assertEquals("CCLX", number.toRoman(260));
+//		assertEquals("CDLXXXV", number.toRoman(485));
+		
+//		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
+//		assertEquals("III", number.toRoman(3));
+//		assertEquals("IV", number.toRoman(4));
+//		assertEquals("V", number.toRoman(5));
+		
+//		assertEquals("C", number.toRoman(100));
+//		assertEquals("CC", number.toRoman(200));
+//		assertEquals("CCL", number.toRoman(250));
+//		assertEquals("MD", number.toRoman(1500));
+//		assertEquals("MMM", number.toRoman(3000));
 	}
 	
 
